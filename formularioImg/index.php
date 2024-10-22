8ea62c113b04da33c1cf03b7acf452e9bec5045e
Diego Sesmero <sesmerov@protonmail.com>
2024-10-22 18:18:03 +0200

    Modificada ubicacion de funciones en index.php

--- formularioImg/index.php
+++ formularioImg/index.php
@@ -1,53 +1,55 @@
 <?php
 
+function verArmas()
+{
+
+    $resultado = "";
+    if (!isset($_POST["armas"])) {
+        $resultado .= "SIN ARMAS";
+    } else {
+        for ($i=0; $i < count($_POST["armas"]) ; $i++) {
+            if($i == count($_POST["armas"])-2){
+                $resultado .= $_POST["armas"][$i] . " y ";
+            }else if ($i == count($_POST["armas"])-1){
+                $resultado .= $_POST["armas"][$i];
+            }else{
+                $resultado .= $_POST["armas"][$i] . ", ";
+            }
+        }
+    }
+    return $resultado;
+}
+
+function getImage()
+{
+    $src = "./manny-calavera.png"; // Imagen por defecto
+
+    if (isset($_FILES['fichero']["name"]) && $_FILES['fichero']['error'] == 0) {
+        $nombreFichero = $_FILES['fichero']['name'];
+        $tipoFichero = $_FILES['fichero']['type'];
+        $tamanioFichero = $_FILES['fichero']['size'];
+        $temporalFichero = $_FILES['fichero']['tmp_name'];
+
+
+        // Verificar que uploads existe y tenemos permisos. Verificar tamaño del archivo y tipo para subir a carpeta uploads. 
+        if ( is_dir("./uploads") && is_writable("./uploads")
+        && $tamanioFichero <= 10000 && $tipoFichero == "image/png") {
+            if (move_uploaded_file($temporalFichero, './uploads/' . $nombreFichero)) {
+                $src = "./uploads/" . $nombreFichero;
+            } 
+        }
+    }
+    return $src;
+}
+
+
+
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     include("captura.html");
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    function verArmas()
-    {
-
-        $resultado = "";
-        if (!isset($_POST["armas"])) {
-            $resultado .= "SIN ARMAS";
-        } else {
-            for ($i=0; $i < count($_POST["armas"]) ; $i++) {
-                if($i == count($_POST["armas"])-2){
-                    $resultado .= $_POST["armas"][$i] . " y ";
-                }else if ($i == count($_POST["armas"])-1){
-                    $resultado .= $_POST["armas"][$i];
-                }else{
-                    $resultado .= $_POST["armas"][$i] . ", ";
-                }
-            }
-        }
-        return $resultado;
-    }
-
-    function getImage()
-    {
-        $src = "./manny-calavera.png"; // Imagen por defecto
-
-        if (isset($_FILES['fichero']["name"]) && $_FILES['fichero']['error'] == 0) {
-            $nombreFichero = $_FILES['fichero']['name'];
-            $tipoFichero = $_FILES['fichero']['type'];
-            $tamanioFichero = $_FILES['fichero']['size'];
-            $temporalFichero = $_FILES['fichero']['tmp_name'];
-
-
-            // Verificar que uploads existe y tenemos permisos. Verificar tamaño del archivo y tipo para subir a carpeta uploads. 
-            if ( is_dir("./uploads") && is_writable("./uploads")
-            && $tamanioFichero <= 10000 && $tipoFichero == "image/png") {
-                if (move_uploaded_file($temporalFichero, './uploads/' . $nombreFichero)) {
-                    $src = "./uploads/" . $nombreFichero;
-                } 
-            }
-        }
-        return $src;
-    }
 
     $nombre =trim(htmlspecialchars( $_POST["nombre"])); //tratamiento inyeccion
     $alias = trim(htmlspecialchars($_POST["alias"])); //tratamiento inyeccion
